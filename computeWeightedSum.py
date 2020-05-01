f8a9025ac1011ee6aa0969f666c834c68232d762
Dipen Yadav <dipenyadav@Dipens-MacBook-Pro.local>
2020-04-30 23:11:52 -0500

    Major changes

--- computeWeightedSum.py
+++
@@ -1,6 +0,0 @@
-# This function computes the weighted sum of each node
-import numpy as np
-
-
-def computeWeightedSum(inputs, weights, bias):
-    return np.sum(inputs * weights) + bias
--- forwardProp.py
+++ forwardProp.py
@@ -1,10 +1,41 @@
-def forwardProp(network,inputs):
-    layerInputs = list(inputs)
-    for layer in network:
-        layerData = network[layer]
-        layerOutputs = []
+from utilities import *
+import numpy as np
 
-        for layerNode in layerData:
-            nodeData = layerData[layerNode]
+# Initialize the network here
+""" Please start by changing the parameters of the Neural Network below """
+# lets start by initializing the variables in the NN as shown below
+numInputs = 6
+numHiddenLayers = 5
+numNodesHidden = [4, 5, 5, 5, 5]
+numNodesOutput = 1
 
-            # Now lets compute the weighted sum at each node in the NN
+my_network = initialize_network(num_inputs=numInputs, num_hidden_layers=numHiddenLayers,
+                                num_nodes_hidden=numNodesHidden, num_nodes_output=numNodesOutput)
+
+
+def forward_propagate(inputs):
+    # global layer_outputs
+    layer_inputs = list(inputs)  # start with the input layer as the input to the first hidden layer
+
+    for layer in my_network:
+
+        layer_data = my_network[layer]
+
+        layer_outputs = []
+        for layer_node in layer_data:
+            node_data = layer_data[layer_node]
+
+            # compute the weighted sum and the output of each node at the same time
+            node_output = node_activation(compute_weighted_sum(layer_inputs, node_data['weights'], node_data['bias']))
+            layer_outputs.append(np.around(node_output[0], decimals=4))
+
+        if layer != 'output':
+            print('The outputs of the nodes in hidden layer number {} is {}'.format(layer.split('_')[1], layer_outputs))
+
+        layer_inputs = layer_outputs  # set the output of this layer to be the input to next layer
+
+    network_predictions = layer_outputs
+    return network_predictions
+
+
+print(my_network)
